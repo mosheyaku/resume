2bb92759a8c4ae95f3da6892493e813783626cf7
Moshe Yaku <mosheyaku@gmail.com>
2024-01-02 12:55:50 +0200

    Several settings have been updated so that the main page can be displayed.

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+
+def start_page(request):
+    return render(request, "resume/index.html")
+
+
