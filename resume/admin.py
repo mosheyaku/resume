857a0847cd56f9e3f03e038cf6aed861d6a9863e
Moshe Yakubov <mosheyaku@gmail.com>
2025-06-23 15:05:58 +0300

    Add skill categories and display skills grouped by category

--- resume/admin.py
+++ resume/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import About, Skill, Project, ContactInfo
+from .models import About, Skill, Project, ContactInfo, SkillCategory
 
 admin.site.register(About)
 admin.site.register(Skill)
+admin.site.register(SkillCategory)
 admin.site.register(Project)
 admin.site.register(ContactInfo)
