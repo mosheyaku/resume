6234896791ad1dcf4ce1b3486704caad64d5b875
mosheyaku <mosheyaku@gmail.com>
2025-07-02 05:24:41 +0300

    Add education model.

--- resume/admin.py
+++ resume/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import About, Skill, Project, ContactInfo, SkillCategory
+from .models import About, Skill, Project, ContactInfo, SkillCategory, Education
 
 admin.site.register(About)
+admin.site.register(Education)
 admin.site.register(Skill)
 admin.site.register(SkillCategory)
 admin.site.register(Project)
